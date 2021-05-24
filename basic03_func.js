7a4d123ceb1d30ab1bcf6226868b8a83dcef711f
skymill2000 <skymill2000@gmail.com>
2021-05-24 15:38:10 +0900

    #work1 완료

--- basic03_func.js
+++ basic03_func.js
@@ -6,6 +6,14 @@ const plus = (p1, p2) => {
   return p1 + p2;
 };
 
+function minus(p1, p2) {
+  return p1 - p2;
+}
+
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
 //#work1
 // minus , div 두가지의 함수를 추가해주세요 ! , minus function, div arrow function 작성 바랍니다.
 
@@ -13,3 +21,5 @@ let result = multi(2, 4);
 console.log(multi(2, 6));
 console.log(result);
 console.log(plus(10, 60));
+console.log(minus(10, 5));
+console.log(div(100, 10));
