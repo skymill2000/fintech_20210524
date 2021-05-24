39072c31ab047e9706533d55a7b7e63d402e2d26
skymill2000 <skymill2000@gmail.com>
2021-05-24 15:29:53 +0900

    #work1 함수 추가하기

---
+++ basic03_func.js
@@ -0,0 +1,15 @@
+function multi(p1, p2) {
+  return p1 * p2;
+}
+
+const plus = (p1, p2) => {
+  return p1 + p2;
+};
+
+//#work1
+// minus , div 두가지의 함수를 추가해주세요 ! , minus function, div arrow function 작성 바랍니다.
+
+let result = multi(2, 4);
+console.log(multi(2, 6));
+console.log(result);
+console.log(plus(10, 60));
