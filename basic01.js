becdbcda6499cf584fac1ff006837b964b166772
skymill2000 <skymill2000@gmail.com>
2021-05-24 15:05:57 +0900

    basic 01 자바스크립트 변수에 관하여

---
+++ basic01.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = "test";
+let floatData = 0.15487;
+
+const c = 3;
+const obj = {
+    name : "test",
+    age : 12,
+}
+
+//in JAVA
+//private String name = "test";
+//private int age = 16;
+//private float height = 168.8;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(floatData);
+console.log(obj);
