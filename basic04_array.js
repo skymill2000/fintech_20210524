daa6dbf8d31fa2f2be3ac7f83439f8e47be0e799
skymill2000 <skymill2000@gmail.com>
2021-05-24 15:43:02 +0900

    배열 선언하고 사용하기

---
+++ basic04_array.js
@@ -0,0 +1,18 @@
+let carArray = [
+  "bmw",
+  "sonata",
+  "tico",
+  123,
+  12.23124,
+  function () {
+    console.log("data");
+  },
+];
+// String [] carNameList = new String[3];
+
+console.log(carArray[0]);
+console.log(carArray[1]);
+console.log(carArray[2]);
+console.log(carArray[3]);
+console.log(carArray[4]);
+carArray[5];
