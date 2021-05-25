4c6d71471908fb830f0e8bb611c070ebd9c60570
skymill2000 <skymill2000@gmail.com>
2021-05-25 16:21:05 +0900

    styled-component 추가 예제 작성

--- fintech/src/component/ListItem.js
+++ fintech/src/component/ListItem.js
@@ -1,12 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 1px solid #ffcfcf;
+  background: #dfdfdf;
+  border-radius: 0.3rem;
+`;
 
 const ListItem = ({ user }) => {
   return (
-    <div>
-      <h1 style={{ color: "red", fontSize: "120px" }}>{user.name}</h1>
+    <Card>
+      <h1>{user.name}</h1>
       <h2>{user.age}</h2>
       <h3>{user.height}</h3>
-    </div>
+    </Card>
   );
 };
 
