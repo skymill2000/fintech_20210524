fec6b1cadcc5c950755db32db99b9201a67e51d7
skymill2000 <skymill2000@gmail.com>
2021-05-25 16:14:26 +0900

    리스트 아이템 컴포넌트 스타일 추가

--- fintech/src/component/ListItem.js
+++ fintech/src/component/ListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const ListItem = ({ user }) => {
   return (
     <div>
-      <h1 style={{ color: "red" }}>{user.name}</h1>
+      <h1 style={{ color: "red", fontSize: "120px" }}>{user.name}</h1>
       <h2>{user.age}</h2>
       <h3>{user.height}</h3>
     </div>
