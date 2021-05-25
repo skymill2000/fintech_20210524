1abc5cdf173654a13b770f02d645bda11e12130d
skymill2000 <skymill2000@gmail.com>
2021-05-25 16:26:02 +0900

    리액트 라우터 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,25 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Welcome from "./component/Welcome";
-import { useState } from "react";
-import ListComponent from "./component/ListComponent";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ListComponet from "./component/ListComponet";
 
-const App = () => {
-  let [userName, setUserName] = useState("유관우");
-
-  const handleChangeName = (e) => {
-    const { value, name } = e.target;
-    setUserName(value);
-  };
-
-  console.log(userName);
+function App() {
   return (
-    <div className="App">
-      <p>{userName} 님 안녕하세요</p>
-      <input onChange={handleChangeName}></input>
-      <ListComponent></ListComponent>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/list" exact>
+          <ListComponet />
+        </Route>
+      </Switch>
+    </Router>
   );
-};
+}
 
 export default App;
