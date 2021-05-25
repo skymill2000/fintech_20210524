c7af36cb42a44e99c8c15868f24fab2be0e40685
skymill2000 <skymill2000@gmail.com>
2021-05-25 16:54:19 +0900

    데이터 표시하기

--- fintech/src/component/AxiosComponent.js
+++ fintech/src/component/AxiosComponent.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+
 const AxiosComponent = () => {
+  const [data, setdata] = useState("");
   const getData = () => {
     axios
       .get("https://naver.com")
       .then(function (response) {
-        // handle success
+        setdata(response.data);
         console.log(response);
       })
       .catch(function (error) {
@@ -20,6 +22,7 @@ const AxiosComponent = () => {
   return (
     <div>
       <button onClick={getData}>데이터 가져오기</button>
+      {data}
     </div>
   );
 };
