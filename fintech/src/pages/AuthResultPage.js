4e1a9855d28a2e446206578ab5bb8ccfc3f0953b
skymill2000 <skymill2000@gmail.com>
2021-05-26 16:57:41 +0900

    토큰 발급 받기 요청 버튼 이벤트 연동 완료

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import Header from "../component/common/Header";
+import AuthButton from "../component/common/AuthButton";
+import axios from "axios";
 
 const AuthResultPage = () => {
   const { search } = useLocation();
   const { code } = queryString.parse(search);
+
+  const getAccessToken = () => {
+    const option = {
+      method: "POST",
+      url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        code: code,
+        client_id: "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF",
+        client_secret: "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy",
+        redirect_uri: "http://localhost:3000/authResult",
+        grant_type: "authorization_code",
+      },
+    };
+    axios(option).then((response) => {
+      console.log(response.data);
+    });
+  };
+
   return (
     <>
+      <Header title={"사용자 발급 토큰 확인"}></Header>
       <p>사용자가 발급받은 사용자 코드는</p>
       <p>{code}</p>
+      <AuthButton title={"토큰받기"} handleClick={getAccessToken}></AuthButton>
     </>
   );
 };
