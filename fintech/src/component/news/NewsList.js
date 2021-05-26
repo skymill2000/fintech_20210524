4f9f0d6a9acd0c6e5ac39e57d8e51c174cb25683
skymill2000 <skymill2000@gmail.com>
2021-05-26 11:33:39 +0900

    뉴스 검색 중간(이벤트 연동 선언)

--- fintech/src/component/news/NewsList.js
+++ fintech/src/component/news/NewsList.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NewsList = ({ news }) => {
-  return <div>{news.map((data) => {})}</div>;
+  return (
+    <div>
+      {news.map((data) => {
+        return <p>{news.title}</p>;
+      })}
+    </div>
+  );
 };
 
 export default NewsList;
--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import Header from "../component/common/Header";
+import NewsList from "../component/news/NewsList";
 import Search from "../component/news/Search";
 
 const NewsPage = () => {
   const [news, setnews] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  const handleChangeInput = () => {};
+  const handleChangeInput = (e) => {
+    //   searchText 변경
+  };
+  const handleClickSearchButton = () => {
+    //   axios 요청 보내기
+  };
 
   return (
     <div>
       <Header title="뉴스검색"></Header>
       {/* Header 컴포넌트 작성 */}
-      <Search></Search>
+      <Search
+        handleChangeSearchInput={handleChangeInput}
+        handleClickSearchButton={handleClickSearchButton}
+      ></Search>
       {/* Search 컴포넌트 작성 */}
-
+      <NewsList news={news}></NewsList>
       {/* NewsList 컴포넌트 작성 */}
     </div>
   );
