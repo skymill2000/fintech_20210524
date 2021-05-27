4359233962f5393e33c9d071492fb79afe999e0f
skymill2000 <skymill2000@gmail.com>
2021-05-27 14:31:56 +0900

    카드 디자인 추가

---
+++ fintech/src/component/list/Card.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  padding: 20px;
+  border: 1px #dfdfdf solid;
+`;
+
+const CardTitle = styled.div`
+  font-size: 1rem;
+`;
+const FintechUseNo = styled.div`
+  font-size: 0.7rem;
+  margin-bottom: 30px;
+`;
+
+const QrButton = styled.button`
+  width: 120px;
+  height: 40px;
+  background: #2959d4;
+  color: white;
+  border: none;
+`;
+
+const BalanceButton = styled.button`
+  width: 120px;
+  height: 40px;
+  background: #2959d4;
+  color: white;
+  border: none;
+`;
+
+const ButtonBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Card = ({ bankName, fintechUseNo, handleQrClick, handleBalaceClick }) => {
+  return (
+    <CardBlock>
+      <CardTitle>{bankName}</CardTitle>
+      <FintechUseNo>{fintechUseNo}</FintechUseNo>
+      <ButtonBlock>
+        <QrButton onClick={handleQrClick}>qr코드</QrButton>
+        <BalanceButton onClick={handleBalaceClick}>잔액조회</BalanceButton>
+      </ButtonBlock>
+    </CardBlock>
+  );
+};
+
+export default Card;
--- fintech/src/pages/ListPage.js
+++ fintech/src/pages/ListPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../component/common/Header";
 import AuthButton from "../component/common/AuthButton";
 import axios from "axios";
+import Card from "../component/list/Card";
 
 const ListPage = () => {
   const [acountList, setAccountList] = useState([]);
@@ -25,13 +26,25 @@ const ListPage = () => {
     });
   };
 
+  const handleQrClick = () => {};
+
+  const handleBalaceClick = () => {};
+
   //getAccountList useEffect 활용해서 불러오기 불러온다음에 acountList set 해주기
 
   return (
     <>
       <Header title="계좌 목록 확인"></Header>
       {acountList.map((account) => {
-        return <p>{account.fintech_use_num}</p>;
+        return (
+          <Card
+            key={account.fintech_use_num}
+            bankName={account.bank_name}
+            fintechUseNo={account.fintech_use_num}
+            handleQrClick={handleQrClick}
+            handleBalaceClick={handleBalaceClick}
+          ></Card>
+        );
       })}
     </>
   );
