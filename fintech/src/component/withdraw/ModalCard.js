c588205feb328059df0ac0a301b6ee231349d9c0
skymill2000 <skymill2000@gmail.com>
2021-05-28 14:27:01 +0900

    사용자 결제 완료

--- fintech/src/component/withdraw/ModalCard.js
+++ fintech/src/component/withdraw/ModalCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const ModalCardBlock = styled.div`
@@ -27,14 +28,56 @@ const WithDrawButton = styled.button`
 
 const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
   // input data 받아온다음에 결제 버튼을 눌렀을때 axios 출금 이체를 바생시키기;
+  const [amount, setamount] = useState("");
+  const handleAmountChange = (e) => {
+    const { value } = e.target;
+    setamount(value);
+  };
+  const genTransId = () => {
+    let countnum = Math.floor(Math.random() * 1000000000) + 1;
+    let transId = "T991599190U" + countnum; //이용기과번호 본인것 입력
+    return transId;
+  };
+
+  const handleClickWithdraw = () => {
+    var sendData = JSON.stringify({
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "7832932596",
+      dps_print_content: "이용료",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "이용료",
+      tran_amt: amount,
+      tran_dtime: "20210528114700",
+      req_client_name: "진상언",
+      req_client_fintech_use_num: fintechUseNo,
+      req_client_num: "HONGGILDONG1234",
+      transfer_purpose: "ST",
+      recv_client_name: "진상언",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "7832932596",
+    });
+
+    const option = {
+      method: "POST",
+      url: "/v2.0/transfer/withdraw/fin_num",
+      headers: {
+        Authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+      data: sendData,
+    };
+    axios(option).then((response) => {
+      console.log(response.data);
+    });
+  };
 
   return (
     <ModalCardBlock>
       <CardTitle>{bankName}</CardTitle>
       <FintechUseNo>{fintechUseNo}</FintechUseNo>
       <p>{tofintechno}에 출금이체를 발생시킵니다.</p>
-      <input></input>
-      <WithDrawButton>결재하기</WithDrawButton>
+      <input onChange={handleAmountChange}></input>
+      <WithDrawButton onClick={handleClickWithdraw}>결재하기</WithDrawButton>
     </ModalCardBlock>
   );
 };
