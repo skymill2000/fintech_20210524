b9da580f0ecb15ce1c85ac8f6ae892e297215fb5
skymill2000 <skymill2000@gmail.com>
2021-05-28 10:33:09 +0900

    qr 코드 생성기 완성

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -1,18 +1,30 @@
 import React from "react";
+import { useLocation } from "react-router";
 import styled from "styled-components";
 import Header from "../component/common/Header";
+import queryString from "query-string";
+
 var QRCode = require("qrcode.react");
 const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 1rem;
 `;
 
 const QrCodePage = () => {
+  const { search } = useLocation();
+  console.log(search);
+  const { finuseno } = queryString.parse(search);
+  console.log(finuseno);
+
   return (
     <>
       <Header title="QR 코드 생성"></Header>
       <QRBlock>
-        <QRCode value="http://facebook.github.io/react/" />
+        <QRCode value={finuseno} />
         {/* finctech 벊호가 value 입력될 수 있게 만들엊 ㅜ세요 */}
+        <p>{finuseno}</p>
       </QRBlock>
     </>
   );
