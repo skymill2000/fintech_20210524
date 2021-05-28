3715b90ae16370a304a98956fb39bb743d96b55e
skymill2000 <skymill2000@gmail.com>
2021-05-28 13:47:01 +0900

    출금 이체 시슯

--- fintech/src/component/withdraw/ModalCard.js
+++ fintech/src/component/withdraw/ModalCard.js
@@ -26,6 +26,8 @@ const WithDrawButton = styled.button`
 `;
 
 const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
+  // input data 받아온다음에 결제 버튼을 눌렀을때 axios 출금 이체를 바생시키기;
+
   return (
     <ModalCardBlock>
       <CardTitle>{bankName}</CardTitle>
