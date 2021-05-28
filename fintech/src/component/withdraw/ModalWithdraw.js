4f595d81460165e98335472674be1a0912d47c24
skymill2000 <skymill2000@gmail.com>
2021-05-28 13:37:06 +0900

    dot 삭제

--- fintech/src/component/withdraw/ModalWithdraw.js
+++ fintech/src/component/withdraw/ModalWithdraw.js
@@ -14,7 +14,7 @@ const ModalWithdrawBlock = styled.div`
 
 const ModalWithdraw = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
